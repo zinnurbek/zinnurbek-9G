915a85d123e1b98459b54d37370007c84c2ab28e
Zinnurbek <isaqjanovkamronbek@gmail.com>
2025-01-21 09:15:36 +0500

    bugungi kodlar

---
+++ DARS.PY/cc.py
@@ -0,0 +1,58 @@
+# mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"] # mevalar ro'yxati (matnlar)
+# narhlar = [12000, 18000, 10900, 22000]
+# print("birinchi meva: " ,mevalar[3].title())
+# print("ikkinchi meva: ",mevalar[-1].upper())
+# print("Sizning bozorligingiz shuncha boldi: ", narhlar[0] + narhlar[2])
+
+
+
+# mevalar = ['olma', 'anjir', 'shaftoli', "o'rik"]
+# mevalar.append("uzum.".title()) # mevalar ga tarvuz qo'shamiz
+# mevalar.append(input("Meva kirit: ")) # mevalar ga tarvuz qo'shamiz
+# print(mevalar)
+
+
+
+
+
+# cars = [] # bo'sh ro'yxat yaratamiz
+# cars.append('Lacetti'.upper()) # ro'yxatga Lacetti mashinasini qo'shamiz
+# cars.append('Nexia 3'.capitalize()) # ro'yxatga Nexia 3 mashinasini qo'shamiz
+# cars.append('Cobalt'.lower())  # ro'yxatga Cobalt  mashinasini qo'shamiz
+# cars.append((input("mashina kirit:>>>")).lower())
+# print(cars)
+
+
+
+
+
+# cars = ['Lacetti', 'Nexia 3', 'Cobalt']
+# cars.insert(0, 'Malibu') # 1-o'ringa yangi qiymat qo'shamiz
+# print(cars)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- dars_r.py
+++
@@ -1,6 +0,0 @@
-from ism import ism
-
-
-yosh = 36
-print(type(ism))  # ism degan o'zgaruvchining turini konsolga chiqaramiz
-print(type(yosh))
